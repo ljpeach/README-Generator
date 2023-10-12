4fb8534e7fdc22329a1e593656c07fc3d0c76dba
Liam Peachey <liamjpeachey@gmail.com>
2023-10-12 12:58:41 -0500

    Updated gitignore to include package-lock and output folder. Imported packages in index.js, added questions for inquirer prompts, added code to write to file (and create subdirectory to organize them) as well as called function to generate markdown. Wrote code in generateMarkdown.js to generate markdown file. Added project description to package.json

--- index.js
+++ index.js
@@ -1,13 +1,75 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown");
+const { Console } = require("console");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+//Title, description, installation instructions, usage information, contribution guidelines, and test instructions
+// Choose license
+// GitHub Username
+// Email address
+const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "Please enter your project title."
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Please enter a description of your project."
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "Please enter installation instructions for your project."
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "Please enter usage information for your project."
+    },
+    {
+        type: "input",
+        name: "contribution",
+        message: "Please describe how others can contribute to your project."
+    },
+    {
+        type: "input",
+        name: "testing",
+        message: "Please enter testing instructions for your project."
+    },
+    {
+        type: "list",
+        name: "license",
+        message: "Please select a license for your project.",
+        choices: ["MIT", "none"]
+    },
+    {
+        type: "input",
+        name: "github",
+        message: "Please enter your github username."
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "Please enter your email address."
+    },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// Table of Contents has functional links (hyperlink to proper section of README)
+function writeToFile(fileName, data) {
+    // console.log(fileName, data);
+    fs.writeFile(fileName, generateMarkdown(data), (err) => err ? console.error(err) : console.log("Successfully written README.md to outs folder."));
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    fs.mkdir("outs", { recursive: true }, (err) => err ? console.error(err) : console.log("Verified output folder."));
+    inquirer.prompt(questions).then(response => writeToFile(`outs/README.md`, response));
+}
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,19 +1,57 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (!license) {
+    return "";
+  }
+  return `![${license}](https://img.shields.io/badge/license-${license}-blue)`;
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (!license) {
+    return "";
+  }
+  return `[${license}](./LICENSE.md)`;
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (!license) {
+    return "";
+  }
+  return `Liscenced under ${renderLicenseLink(license)}`;
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-
+  ${renderLicenseBadge(data.license)}
+  ## Description
+  ${data.description}
+  ## Table of Contents
+  - [Description](#description)
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contributing](#contributing)
+  - [Testing](#testing)
+  - [Questions](#questions)
+  ## Installation
+  ${data.installation}
+  ## Usage
+  ${data.usage}
+  ## License
+  ${renderLicenseSection(data.license)}
+  ## Contributing
+  ${data.contribution}
+  ## Testing
+  ${data.testing}
+  ## Questions
+  The developer can be found at [their GitHub Profile](https://www.github.com/${data.github})
+  Direct any additional questions to ${data.email}.
 `;
 }
 
